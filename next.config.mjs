ef3fa2777098e08ea16a74eaa1c3dc684fd043e8
Rampagerrr <fanspager@gmail.com>
2025-04-25 09:26:33 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.noitatnemucod.net','artworks.thetvdb.com','s4.anilist.co','https://media.kitsu.app'], // Add your image domain here
+    domains: [
+      'cdn.noitatnemucod.net',
+      'artworks.thetvdb.com',
+      's4.anilist.co',
+      'media.kitsu.app' // ✅ No protocol
+    ],
   },
 };
 
